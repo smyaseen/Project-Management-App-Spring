e69cfd111111561910e86d7fdc493a11f8dd2e6b
smy <smyaseen164@gmail.com>
2021-02-05 23:04:44 +0500

    added @Entity annotation

--- src/main/java/org/smy/pma/entities/Project.java
+++ src/main/java/org/smy/pma/entities/Project.java
@@ -1,9 +1,11 @@
 package org.smy.pma.entities;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Project {
 
     @Id
