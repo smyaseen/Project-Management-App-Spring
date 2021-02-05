5d12a72fe48dae1e9c6b53c025e71b6c3f5e1ee3
smy <smyaseen164@gmail.com>
2021-02-05 19:23:22 +0500

    Initial commit

---
+++ src/test/java/org/smy/pma/PmaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.smy.pma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
