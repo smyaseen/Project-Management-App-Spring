451fdf6151dd3ffe35c8c33fb6712df36b4c3f72
smy <smyaseen164@gmail.com>
2021-02-09 01:11:10 +0500

    overridden findAll() to make a List

--- src/main/java/org/smy/pma/dao/EmployeeRepository.java
+++ src/main/java/org/smy/pma/dao/EmployeeRepository.java
@@ -4,6 +4,12 @@ import org.smy.pma.entities.Employee;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+    @Override
+    List<Employee> findAll();
+
 }
--- src/main/java/org/smy/pma/dao/ProjectRepository.java
+++ src/main/java/org/smy/pma/dao/ProjectRepository.java
@@ -1,9 +1,16 @@
 package org.smy.pma.dao;
 
+import org.smy.pma.entities.Employee;
 import org.smy.pma.entities.Project;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    List<Project> findAll();
+
 }
