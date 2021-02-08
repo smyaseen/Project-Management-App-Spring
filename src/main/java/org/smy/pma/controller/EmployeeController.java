006b945ac4c8607f3d5328780ead5b1da304ffdb
smy <smyaseen164@gmail.com>
2021-02-08 23:44:53 +0500

    created an employee controller

---
+++ src/main/java/org/smy/pma/controller/EmployeeController.java
@@ -0,0 +1,32 @@
+package org.smy.pma.controller;
+import org.dom4j.rule.Mode;
+import org.smy.pma.dao.EmployeeRepository;
+import org.smy.pma.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @GetMapping("/new-employee")
+    public String displayEmployeeForm(Model model) {
+        Employee aEmployee = new Employee();
+        model.addAttribute("employee",aEmployee);
+        return "new-employee";
+    }
+
+    @PostMapping("/save")
+    public String createEmployee(Employee employee, Model model) {
+        employeeRepository.save(employee);
+        return "redirect:/employee/new-employee";
+    }
+
+}
