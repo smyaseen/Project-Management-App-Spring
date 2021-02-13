bec01075b4bbe22723dcf9a11b567c6aff17f86d
smy <smyaseen164@gmail.com>
2021-02-13 23:49:11 +0500

    added one to many annotation for employees

--- src/main/java/org/smy/pma/entities/Project.java
+++ src/main/java/org/smy/pma/entities/Project.java
@@ -1,9 +1,7 @@
 package org.smy.pma.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Project {
@@ -13,7 +11,11 @@ public class Project {
     private Long projectId;
     private String name;
     private String stage;
+
     private String description;
+
+    @OneToMany(mappedBy = "project")
+    private List<Employee> employees;
 
     public Project() {
 
@@ -23,6 +25,14 @@ public class Project {
         this.name = name;
         this.stage = stage;
         this.description = description;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
     }
 
     public Long getProjectId() {
