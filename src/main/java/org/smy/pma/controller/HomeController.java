15da15214146b067f34bf47947f7dfbbb5e50f74
smyaseen <smyaseen164@gmail.com>
2021-03-06 22:19:34 +0500

    replaced employeesList with employeeProject

--- src/main/java/org/smy/pma/controller/HomeController.java
+++ src/main/java/org/smy/pma/controller/HomeController.java
@@ -2,6 +2,7 @@ package org.smy.pma.controller;
 
 import org.smy.pma.dao.EmployeeRepository;
 import org.smy.pma.dao.ProjectRepository;
+import org.smy.pma.dto.EmployeeProject;
 import org.smy.pma.entities.Employee;
 import org.smy.pma.entities.Project;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,9 +24,9 @@ public class HomeController {
     @GetMapping("/")
     public String displayHome(Model model) {
         List<Project> projects = projectRepository.findAll();
-        List<Employee> employees = employeeRepository.findAll();
+        List<EmployeeProject> employeeProjects = employeeRepository.getEmployeeProjects();
         model.addAttribute("projectsList",projects);
-        model.addAttribute("employeesList",employees);
+        model.addAttribute("employeesProjects",employeeProjects);
         return "main/home";
     }
 
