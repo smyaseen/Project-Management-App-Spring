0057d50a4b4d1b3299662e0dd639564c80426dc4
smyaseen <smyaseen164@gmail.com>
2021-03-06 22:18:49 +0500

    interface for employee project join table

---
+++ src/main/java/org/smy/pma/dto/EmployeeProject.java
@@ -0,0 +1,10 @@
+package org.smy.pma.dto;
+
+public interface EmployeeProject {
+
+    // Need to have the property names begin with
+    public String getFirstName();
+    public String getLastName();
+    public int getProjectCount();
+
+}
