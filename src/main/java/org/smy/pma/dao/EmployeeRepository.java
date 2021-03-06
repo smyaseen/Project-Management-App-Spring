75f8c562afd505c26fb84946fb2527c540d00524
smyaseen <smyaseen164@gmail.com>
2021-03-06 22:18:21 +0500

    query to get employee first name,last name and no. of projects

--- src/main/java/org/smy/pma/dao/EmployeeRepository.java
+++ src/main/java/org/smy/pma/dao/EmployeeRepository.java
@@ -1,6 +1,8 @@
 package org.smy.pma.dao;
 
+import org.smy.pma.dto.EmployeeProject;
 import org.smy.pma.entities.Employee;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +13,10 @@ public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
     @Override
     List<Employee> findAll();
+
+    @Query(nativeQuery = true, value="Select e.first_name as firstName,e.last_name as lastName, COUNT(pe.employee_id) as projectCount " +
+            "From employee e left join project_employee pe ON pe.employee_id = e.employee_id " +
+            "GROUP BY  e.first_name,e.last_name ORDER BY 3 DESC")
+    List<EmployeeProject> getEmployeeProjects();
 
 }
