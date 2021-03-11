b0d861bdcd014db33030ba96f52042188e607688
smy <smyaseen164@gmail.com>
2021-03-11 16:27:05 +0500

    added code for retrieving project status and parsing into json to use in JS

--- src/main/java/org/smy/pma/controller/HomeController.java
+++ src/main/java/org/smy/pma/controller/HomeController.java
@@ -1,16 +1,20 @@
 package org.smy.pma.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.smy.pma.dao.EmployeeRepository;
 import org.smy.pma.dao.ProjectRepository;
 import org.smy.pma.dto.EmployeeProject;
-import org.smy.pma.entities.Employee;
+import org.smy.pma.dto.ChartData;
 import org.smy.pma.entities.Project;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HomeController {
@@ -22,9 +26,20 @@ public class HomeController {
     EmployeeRepository employeeRepository;
 
     @GetMapping("/")
-    public String displayHome(Model model) {
+    public String displayHome(Model model) throws JsonProcessingException {
+
+        Map<String,Object> map = new HashMap<>();
+
         List<Project> projects = projectRepository.findAll();
         List<EmployeeProject> employeeProjects = employeeRepository.getEmployeeProjects();
+        List<ChartData> projectData = projectRepository.getProjectStatus();
+
+        //Converting projectData object into JSON structure for use in JS
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(projectData);
+
+        model.addAttribute("projectStatusCnt",jsonString);
+
         model.addAttribute("projectsList",projects);
         model.addAttribute("employeesProjects",employeeProjects);
         return "main/home";
