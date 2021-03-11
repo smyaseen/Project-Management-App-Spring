f91db50f376d91b145b841b07d4aa2c050d53d91
smy <smyaseen164@gmail.com>
2021-03-11 16:25:22 +0500

    added a native query and method to get no. of project statuses

--- src/main/java/org/smy/pma/dao/ProjectRepository.java
+++ src/main/java/org/smy/pma/dao/ProjectRepository.java
@@ -1,7 +1,8 @@
 package org.smy.pma.dao;
 
-import org.smy.pma.entities.Employee;
+import org.smy.pma.dto.ChartData;
 import org.smy.pma.entities.Project;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +14,6 @@ public interface ProjectRepository extends CrudRepository<Project, Long> {
     @Override
     List<Project> findAll();
 
+    @Query(nativeQuery = true, value = "SELECT stage as label, Count(*) as value FROM project GROUP BY stage")
+    List<ChartData> getProjectStatus();
 }
