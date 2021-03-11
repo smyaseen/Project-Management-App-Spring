6f6f7c41f99ebdc1ad9cb4c01256984676505b22
smy <smyaseen164@gmail.com>
2021-03-11 16:24:32 +0500

    nothing changed

--- src/main/java/org/smy/pma/PmaApplication.java
+++ src/main/java/org/smy/pma/PmaApplication.java
@@ -1,17 +1,11 @@
-//manjaro
+//windows
 package org.smy.pma;
 
 import org.smy.pma.dao.EmployeeRepository;
 import org.smy.pma.dao.ProjectRepository;
-import org.smy.pma.entities.Employee;
-import org.smy.pma.entities.Project;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class PmaApplication {
--- src/main/java/org/smy/pma/entities/Employee.java
+++ src/main/java/org/smy/pma/entities/Employee.java
@@ -66,4 +66,5 @@ public class Employee {
     public void setEmail(String email) {
         this.email = email;
     }
+
 }
