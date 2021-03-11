dbb558d6ddaf4e07a6ea6f67b4c97772fbbca520
smy <smyaseen164@gmail.com>
2021-03-11 17:41:59 +0500

    added code to take chartData, parse it into JSON and display the values on chart

--- src/main/resources/static/js/myChart.js
+++ src/main/resources/static/js/myChart.js
@@ -1,27 +1,45 @@
+
+var chartDataStr = decodeHtml(chartData);
+var chartJSONArray = JSON.parse(chartDataStr);
+
+var arrayLength = chartJSONArray.length;
+
+var numericData = [];
+var labelData = [];
+
+for (var i=0 ; i < arrayLength ; i++) {
+    numericData[i] = chartJSONArray[i].value;
+    labelData[i] = chartJSONArray[i].label;
+}
+
+
+
+
 var myChart = new Chart(document.getElementById("myPieChart"), {
     type: 'pie',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: labelData,
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Project Statuses',
+            data: numericData,
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255, 206, 86, 0.2)'
             ],
             borderWidth: 1
         }]
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Project Statuses'
+        }
     }
 });
+
+function decodeHtml(html) {
+    var txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+}
